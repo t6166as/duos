185441e265197253bbab132eda8abf6f4ee93736
j1axs01 <j1axs01@node7.awishkar.com>
2017-08-21 20:16:01 -0500

    second commit

--- abbv.py
+++ abbv.py
@@ -9,15 +9,16 @@ def parse_args():
 
 def abbvtool(str):
 	ac = ''
-	for i in str.split(' '):
-		if len(i) > 3 :
-			ac = ac+i[0]
+	try:
+		for i in str.split(' '):
+			if len(i) > 3 :
+				ac = ac+i[0]
+	except:
+		pass
 	return ac.upper()
 
 def main():
         args = parse_args()
-        print(abbvtool(args.string))
-
 
 if __name__ == "__main__" :
 	main()
---
+++ excelp.py
@@ -0,0 +1,35 @@
+#!/usr/local/bin/python3
+import xlrd as exc
+import csv
+import argparse
+from abbv import abbvtool
+
+def parse_args():
+	parser=argparse.ArgumentParser( description ='supply file that need to be parsed')
+	parser.add_argument('--filename',required=True)
+	parser.add_argument('--outputname',required=True)
+	return parser.parse_args()
+
+
+def csvconvert(filename,outputname):
+	workbook = exc.open_workbook(filename)
+	intial_li=[]
+	for i in workbook.sheet_names():
+		processing_sheet=i
+		worksheet = workbook.sheet_by_name(processing_sheet)
+		for j in range(0, worksheet.nrows):
+			acronym=abbvtool(worksheet.cell_value(j,3))
+			initial_li=intial_li.append([i]+worksheet.row_values(j)+[acronym]) 
+	#print(intial_li)
+	with open(outputname, "w") as f:
+    		writer = csv.writer(f,delimiter='~')
+    		writer.writerows(intial_li)
+
+
+def main():
+	args = parse_args()
+	csvconvert(args.filename,args.outputname)
+
+
+if __name__ == "__main__":
+	main()	
